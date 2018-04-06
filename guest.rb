be49528804fdd21958e8e84b2d9c1ce698167bb2
omikron15 <omikron15@hotmail.co.uk>
2018-04-06 13:01:34 +0100

    Bassic attributes tests for guest class all passing. 3/3

--- guest.rb
+++ guest.rb
@@ -1,3 +1,21 @@
 class Guest
 
+    def initialize(name, favourite_song, money)
+        @name = name
+        @favorite_song = favourite_song
+        @money = money
+    end #End of initialize function
+
+    def name()
+        return @name
+    end #End of name function
+
+    def favourite_song()
+        return @favorite_song
+    end #End of favorite_song function
+
+    def money()
+        return @money
+    end #End of money function
+
 end #End of Guest class
