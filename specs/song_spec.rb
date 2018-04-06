6e3bd88fa647cc512a0ad1620a9d7b9f96086915
omikron15 <omikron15@hotmail.co.uk>
2018-04-06 12:37:25 +0100

    Tests for basic song parameters writen with zero passes

--- specs/song_spec.rb
+++ specs/song_spec.rb
@@ -5,4 +5,20 @@ require_relative("../song.rb")
 
 class SongTest < Minitest::Test
 
+  def setup()
+    @song1 = Song.new("Name", "Artist", "Line from song")
+  end #end of setup function
+
+  def test_get_song_name()
+      assert_equal("Name", @song1.name())
+  end
+
+  def test_get_song_artist()
+      assert_equal("Artist", @song1.artist())
+  end
+
+  def test_get_song_line()
+      assert_equal("Line from song", @song1.song_line())
+  end
+
 end #End of SongTest class
