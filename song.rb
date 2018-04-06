3913257b83411365945492c220a63216c0de2243
omikron15 <omikron15@hotmail.co.uk>
2018-04-06 12:42:28 +0100

    Tests for basic song parameters passed. 3 of 3.

--- song.rb
+++ song.rb
@@ -1,3 +1,23 @@
 class Song
 
+  def initialize(name, artist, song_line)
+
+    @name = name
+    @artist = artist
+    @song_line = song_line
+
+  end #End of initialize function
+
+  def name
+      return @name
+  end #End of name function
+
+  def artist
+      return @artist
+  end #End of artist function
+
+  def song_line
+      return @song_line
+  end ##End of song_line function
+
 end #End of Song class
